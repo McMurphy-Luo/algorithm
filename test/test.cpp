52381533fd4267a1efde2cd28e4f63d94acd4067
McMurphy-Luo <McMurphy.Luo@gmail.com>
2017-10-21 17:14:17 +0800

    temporary commit

--- test/test.cpp
+++ test/test.cpp
@@ -1,4 +1,4 @@
-﻿//
+//
 // Created by luojiayi on 5/18/17.
 //
 #include <memory>
@@ -9,9 +9,23 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
-    assert(false);
-    size_t wide_string_buf_required = MultiByteToWideChar(CP_UTF8, 0, u8"good", -1, nullptr, 0);
+    SYSTEM_INFO sys_info;
+    GetSystemInfo(&sys_info);
 
-    cout << wide_string_buf_required;
+    printf("Hardware information: \n");
+    printf("  OEM ID: %u\n", sys_info.dwOemId);
+    printf("  Number of processors: %u\n",
+        sys_info.dwNumberOfProcessors);
+    printf("  Page size: %u\n", sys_info.dwPageSize);
+    printf("  Processor type: %u\n", sys_info.dwProcessorType);
+    printf("  Minimum application address: %lx\n",
+        sys_info.lpMinimumApplicationAddress);
+    printf("  Maximum application address: %lx\n",
+        sys_info.lpMaximumApplicationAddress);
+    printf("  Active processor mask: %u\n",
+        sys_info.dwActiveProcessorMask);
+    printf(" dwAllocationGranularity is %u\n", sys_info.dwAllocationGranularity);
+
+    
     cin.get();
 }
