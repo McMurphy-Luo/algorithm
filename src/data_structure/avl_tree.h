f8b2c1040a43d45e3dcc80b7d0d1b7989faeb70a
McMurphy.Luo <McMurphy.Luo@zoom.us>
2019-04-15 17:07:52 +0800

    [sip][windows] minor changes

--- src/data_structure/avl_tree.h
+++ src/data_structure/avl_tree.h
@@ -10,103 +10,103 @@
 
 namespace algorithm
 {
-    /*
-     * AVL tree node do not have color, their color are all set black.
-     * 
-     */
-    namespace data_structure
-    {
-        template<typename KeyType, typename ValueType, int(*Comparator)(const KeyType&, const KeyType&)>
-        class AVLTree {
-        public:
-            typedef TreeNode<KeyType, ValueType> node;
-            typedef typename node::key_type key_type;
-            typedef typename node::value_type value_type;
+  namespace data_structure
+  {
+    template<typename KeyType, typename ValueType, typedef Comparator>
+    class AVLTree {
+    public:
+      typedef typename KeyType key_type;
+      typedef typename ValueType value_type;
 
-        public:
-            AVLTree() :root_(nullptr)
-            {
+    private:
+      struct Node {
+        key_type key;
+        value_type value;
+        node *parent;
+        node *left;
+        node *right;
+        size_t height;
+      };
 
-            }
+    public:
+      AVLTree() :root_(nullptr) {
 
-            AVLTree(const AVLTree& another)
-            {
+      }
 
-            }
+      AVLTree(const AVLTree& another) {
 
-            AVLTree& operator=(const AVLTree& rhs)
-            {
-                if (this == &rhs)
-                {
-                    return *this;
-                }
+      }
 
-                return *this;
-            }
+      AVLTree& operator=(const AVLTree& rhs) {
+        if (this == &rhs) {
+            return *this;
+        }
+        return *this;
+      }
 
-            ~AVLTree()
-            {
+      ~AVLTree() {
 
-            }
+      }
 
-            void put(const key_type& key, const value_type& value)
-            {
-                node* find_result = fuzzy_find(root_, key);
-                if (!find_result) {
-                    root_ = new node(key, value, nullptr, nullptr, NodeColor::black);
-                }
-                int compare_result = Comparator(key, find_result->key);
-                if (compare_result == 0) {
-                    find_result->value = value;
-                }
-                if (compare_result > 0) {
-                    find_result->right = new node(key, value, nullptr, nullptr, NodeColor::black);
-                }
-                if (compare_result < 0) {
-                    find_result->left = new node(key, value, nullptr, nullptr, NodeColor::black);
-                }
-                assert(false);
-            }
+      void put(const key_type& key, const value_type& value)
+      {
+        node* find_result = fuzzy_find(root_, key);
+        if (!find_result) {
+          root_ = new node(key, value, nullptr, nullptr, NodeColor::black);
+        }
+        int compare_result = Comparator(key, find_result->key);
+        if (compare_result == 0) {
+          find_result->value = value;
+        }
+        if (compare_result > 0) {
+          find_result->right = new node(key, value, nullptr, nullptr, NodeColor::black);
+        }
+        if (compare_result < 0) {
+          find_result->left = new node(key, value, nullptr, nullptr, NodeColor::black);
+        }
+        assert(false);
+      }
 
-            value_type* remove(const key_type& key)
-            {
+      value_type* remove(const key_type& key)
+      {
 
-            }
+      }
 
 
-        protected:
-            void blance_node(node* which)
-            {
-                size_type left_child_height = height(which->left);
-                size_type right_child_height = height(which->right);
-                int child_height_diff = left_child_height - right_child_height;
-            }
+    protected:
+      void blance_node(node* which)
+      {
+        size_type left_child_height = height(which->left);
+        size_type right_child_height = height(which->right);
+        int child_height_diff = left_child_height - right_child_height;
+      }
 
-            node* fuzzy_find(node* current, const key_type& key)
-            {
-                if (!current) {
-                    return nullptr;
-                }
-                int compare_result = Comparator(key, current->key);
-                if (compare_result > 0) {
-                    if (!(current->right)) {
-                        return current;
-                    }
-                    return find(current->right, key);
-                }
-                if (compare_result < 0) {
-                    if (!(current->left)) {
-                        return current;
-                    }
-                    return find(current->left, key);
-                }
-                return current;
-            }
+      node* fuzzy_find(node* current, const key_type& key)
+      {
+        if (!current) {
+          return nullptr;
+        }
+        int compare_result = Comparator(key, current->key);
+        if (compare_result > 0) {
+          if (!(current->right)) {
+              return current;
+          }
+          return find(current->right, key);
+        }
+        if (compare_result < 0) {
+          if (!(current->left)) {
+              return current;
+          }
+          return find(current->left, key);
+        }
+        return current;
+      }
 
-        private:
-            node* root_;
-        };
-    }
+    private:
+      node* root_;
+      Comparator comparator_;
+    };
+  }
 }
 
 #endif //ALGORITHM_DATA_STRUCTURE_AVL_TREE_H
---
+++ src/sort/insertion_sort.h
@@ -0,0 +1,15 @@
+#ifndef ALGORITHM_SORT_INSERTION_SORT_H
+#define ALGORITHM_SORT_INSERTION_SORT_H
+
+namespace algorithm
+{
+  namespace sort
+  {
+    
+
+
+  }
+}
+
+
+#endif // ALGORITHM_SORT_INSERTION_SORT_H
