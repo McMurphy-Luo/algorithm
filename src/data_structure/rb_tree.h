e2c4c379618cd118842d9108cc6ae5ec2b75edd3
McMurphy Luo <McMurphy.Luo@gmail.com>
2019-05-18 20:20:08 +0800

    Modify the red black tree InsertFix implementation. Let it compile

--- src/data_structure/rb_tree.h
+++ src/data_structure/rb_tree.h
@@ -19,7 +19,7 @@ namespace data_structure
 template<typename KeyType, typename ValueType, typename Comparator>
 class RBTree
 {
-private:
+public:
   enum class NodeColor
   {
     Black,
@@ -83,6 +83,10 @@ public:
   size_t Size() const
   {
     return size_;
+  }
+
+  const Node* Root() const {
+    return root_;
   }
 
   void Put(const KeyType& key, const ValueType& value)
@@ -170,46 +174,93 @@ public:
     }
     assert(!should_before && !should_after);
     // The target node that is required to be removed is found.
-    std::shared_ptr<ValueType> result;
     if (!maybe_result->left && !maybe_result->right) {
-      Node* parent = nullptr;
       if (!maybe_result->parent) {
         assert(maybe_result == root_);
         root_ = nullptr;
       }
       else if(maybe_result->parent->left == maybe_result) {
         maybe_result->parent->left = nullptr;
-        parent = maybe_result->parent;
       }
       else if (maybe_result->parent->right == maybe_result) {
         maybe_result->parent->right = nullptr;
-        parent = maybe_result->parent;
       }
-      result = std::make_shared<ValueType>(maybe_result->value);
-      if (parent) {
-        RemoveFix(parent);
+      if (maybe_result->parent) {
+        RemoveFix(maybe_result->parent);
       }
+      std::shared_ptr<ValueType> result = std::make_shared<ValueType>(maybe_result->value);
+      delete maybe_result;
       --size_;
       return result;
     }
     else if (maybe_result->left && !maybe_result->right) {
-
+      maybe_result->left->parent = maybe_result->parent;
+      if (!maybe_result->parent) {
+        assert(maybe_result == root_);
+        root_ = maybe_result->left;
+      }
+      else if (maybe_result->parent->left == maybe_result) {
+        maybe_result->parent->left = maybe_result->left;
+      }
+      else if (maybe_result->parent->right == maybe_result) {
+        maybe_result->parent->right = maybe_result->left;
+      }
+      std::shared_ptr<ValueType> result = std::make_shared<ValueType>(maybe_result->value);
+      delete maybe_result;
+      --size_;
+      RemoveFix(maybe_result->left);
+      return result;
     }
     else if (!maybe_result->left && maybe_result->right) {
-      
+      Node* successor = nullptr;
+      maybe_result->right->parent = maybe_result->parent;
+      if (!maybe_result->parent) {
+        assert(maybe_result == root_);
+        root_ = maybe_result->right;
+      }
+      else if (maybe_result->parent->left == maybe_result) {
+        maybe_result->parent->left = maybe_result->right;
+      }
+      else if (maybe_result->parent->right == maybe_result) {
+        maybe_result->parent->right = maybe_result->right;
+      }
+      std::shared_ptr<ValueType> result = std::make_shared<ValueType>(maybe_result->value);
+      delete maybe_result;
+      --size_;
+      RemoveFix(maybe_result->right);
+      return result;
     }
     else {
       assert(maybe_result->left && maybe_result->right);
-
+      Node* successor = maybe_result->right;
+      while (successor->left) {
+        successor = successor->left;
+      }
+      if (successor->right) {
+        successor->right->parent = successor->parent;
+      }
+      if (successor->parent->left == successor) {
+        successor->parent->left = successor->right;
+      }
+      else if (successor->parent->right == successor) {
+        successor->parent->right = successor->right;
+      }
+      else {
+        assert(false);
+      }
+      Node* node_to_be_rebalanced = successor->parent;
+      if (node_to_be_rebalanced == maybe_result) {
+        node_to_be_rebalanced = successor;
+      }
+      successor->parent = maybe_result->parent;
+      successor->left = maybe_result->left;
+      successor->right = maybe_result->right;
+      successor->color = maybe_result->color;
+      std::shared_ptr<ValueType> result = std::make_shared<ValueType>(maybe_result->value);
+      delete maybe_result;
+      --size_;
+      RemoveFix(node_to_be_rebalanced);
     }
-
-    Node* successor = detach(maybe_result);
-    if (maybe_result == root_) {
-      root_ = successor;
-    }
-    value_type* result_value = new value_type(maybe_result->value);
-    delete maybe_result;
-    return result_value;
   }
 
   void Clear()
@@ -271,17 +322,64 @@ private:
     return result;
   }
 
+  void LeftRotate(Node* target)
+  {
+    assert(target);
+    assert(target->right);
+    Node* node_to_be_lifted = target->right;
+    target->right = node_to_be_lifted->left;
+    if (target->right) {
+      target->right->parent = target;
+    }
+    node_to_be_lifted->left = target;
+    node_to_be_lifted->parent = target->parent;
+    target->parent = node_to_be_lifted;
+    if (node_to_be_lifted->parent) {
+      if (node_to_be_lifted->parent->left == target) {
+        node_to_be_lifted->parent->left = node_to_be_lifted;
+      }
+      // Never use else even though it is faster, In my opinion
+      if (node_to_be_lifted->parent->right == target) {
+        node_to_be_lifted->parent->right = node_to_be_lifted;
+      }
+    }
+    return;
+  }
+
+  void RightRotate(Node* target)
+  {
+    assert(target);
+    assert(target->left);
+    Node* node_to_be_lifted = target->left;
+    target->left = node_to_be_lifted->right;
+    if (target->left) {
+      target->left->parent = target;
+    }
+    node_to_be_lifted->right = target;
+    node_to_be_lifted->parent = target->parent;
+    target->parent = node_to_be_lifted;
+    if (node_to_be_lifted->parent) {
+      if (node_to_be_lifted->parent->left == target) {
+        node_to_be_lifted->parent->left = node_to_be_lifted;
+      }
+      if (node_to_be_lifted->parent->right == target) {
+        node_to_be_lifted->parent->right = node_to_be_lifted;
+      }
+    }
+    return;
+  }
+
   void InsertFix(Node* which)
     // "which" may be the new inserted node or node on the path from the new node to root
     // In double red case
   {
-    assert(which->color == NodeColor::red);
-    if (which == root_ && which->color == NodeColor::red) {
-      which->color = NodeColor::black;
+    assert(which->color == NodeColor::Red);
+    if (which == root_ && which->color == NodeColor::Red) {
+      which->color = NodeColor::Black;
       return;
     }
 
-    if (which->parent->color == NodeColor::black) {
+    if (which->parent->color == NodeColor::Black) {
       // case 1 hit: parent of the new inserted node is black node
       // all red black tree property remains, so do nothing, insert fix finished.
       return;
@@ -289,9 +387,9 @@ private:
     // from now, new_node->parent is red node and node->parent->parent is always exists because root node
     // is black according to red black tree property, so new_node->parent must not be root node,
     // so that new_node->parent->parent must be exists.
-    node* parent = which->parent;
-    node* grand_parent = parent->parent;
-    node* uncle;
+    Node* parent = which->parent;
+    Node* grand_parent = parent->parent;
+    Node* uncle = nullptr;
     if (grand_parent->left == parent) {
       uncle = grand_parent->right;
     }
@@ -299,37 +397,37 @@ private:
     if (grand_parent->right == parent) {
       uncle = grand_parent->left;
     }
-    if (uncle && uncle->color == NodeColor::red) {
+    if (uncle && uncle->color == NodeColor::Red) {
       // case 2 hit: parent of the new node is red node
       // uncle of the new node is red node
-      uncle->color = NodeColor::black;
-      parent->color = NodeColor::black;
-      grand_parent->color = NodeColor::red;
-      return insert_fix(grand_parent);
+      uncle->color = NodeColor::Black;
+      parent->color = NodeColor::Black;
+      grand_parent->color = NodeColor::Red;
+      return InsertFix(grand_parent);
     }
 
     // from now, parent node is red node, and uncle node is black node
     // or uncle node is null
     if (parent == grand_parent->left) {
       if (which == parent->right) {
-        left_rotate(parent);
+        LeftRotate(parent);
       }
-      right_rotate(grand_parent);
+      RightRotate(grand_parent);
       // from now parent and child are changed, these two pointer loses their meanings
       // One thing is sure that the grand_parent replace the position of uncle previously
-      grand_parent->color = NodeColor::red;
-      grand_parent->parent->color = NodeColor::black;
-      grand_parent->parent->left->color = NodeColor::red;
+      grand_parent->color = NodeColor::Red;
+      grand_parent->parent->color = NodeColor::Black;
+      grand_parent->parent->left->color = NodeColor::Red;
     }
 
     if (parent == grand_parent->right) {
       if (which == parent->left) {
-        right_rotate(parent);
+        RightRotate(parent);
       }
-      left_rotate(grand_parent);
-      grand_parent->color = NodeColor::red;
-      grand_parent->parent->color = NodeColor::black;
-      grand_parent->parent->right->color = NodeColor::red;
+      LeftRotate(grand_parent);
+      grand_parent->color = NodeColor::Red;
+      grand_parent->parent->color = NodeColor::Black;
+      grand_parent->parent->right->color = NodeColor::Red;
     }
     if (root_ == grand_parent) {
       root_ = grand_parent->parent;
